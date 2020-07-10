3c47fccea2ae11fe3494be810739cbf825b73df0
雷诺 <7774273+lei-nuo@user.noreply.gitee.com>
2020-07-10 16:25:40 +0800

    Site updated: 2020-07-10 16:25:39

--- dist/music.js
+++ dist/music.js
@@ -6,7 +6,7 @@ const ap = new APlayer({
       {
         name: "一直很安静",
         artist: '阿桑',
-        url: 'https://sharefs.yun.kugou.com/202007071440/77022cfac0829a00a00a020210cfae70/G200/M04/0A/0D/qJQEAF5xVWaAVulBAD1HQBZkNcw546.mp3',
+        url: 'https://sharefs.yun.kugou.com/202007101621/5074989913f0b585e9d3bebdec4d8372/G200/M04/0A/0D/qJQEAF5xVWaAVulBAD1HQBZkNcw546.mp3',
         cover: 'https://p3fx.kgimg.com/stdmusic/20171016/20171016101203221753.jpg',  
       }
     ]
